4c7c4a326124b9859b4c828fd23d2d0a3e158776
cuzon <128647017+cuzon@users.noreply.github.com>
2023-03-22 18:55:39 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+a=float(input()) # int() pro computador entender que é um número, float() pro computador entender que é um número com virgula
+c=input()
+b=float(input())
+
+
+if c=="*":
+    print(a*b) # print() pro computador mandar uma mensagem para mim e resolver a questão
+
+elif c=="/": # else+if=elif
+    print(a/b)
+elif c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
