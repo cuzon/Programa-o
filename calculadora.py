961c6ec9580e0b159d3485cd7e110c5bda9c102f
cuzon <128647017+cuzon@users.noreply.github.com>
2023-03-29 18:51:51 -0300

    Add files via upload
    uma calculadora 2.0, muito pica

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,16 @@
+print("Digite um número pica: ")
+
 a=float(input()) # int() pro computador entender que é um número, float() pro computador entender que é um número com virgula
+print("Qual operação fodona que você quer?")
 c=input()
+print("Digite um número pica: ")
 b=float(input())
 
+c=c.lower()
 
+if c=="ao quadrado":
+    print(a**2)
+    quit()
 if c=="*":
     print(a*b) # print() pro computador mandar uma mensagem para mim e resolver a questão
 
@@ -12,3 +20,5 @@ elif c=="+":
     print(a+b)
 elif c=="-":
     print(a-b)
+elif c=="elevado":
+    print(a**b)
