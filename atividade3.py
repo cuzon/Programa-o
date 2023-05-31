fa3e0680dd19217663798030f3d7083a1e3d6f2a
cuzon <128647017+cuzon@users.noreply.github.com>
2023-05-31 17:52:15 -0300

    Atividade Avaliativa-Pedro Alles
    maldade, tive q botar aspas em todos os nomes.

---
+++ atividade3.py
@@ -0,0 +1,10 @@
+#Objetivo: Criar um programa que organize uma lista de nomes em ordem alfabetica
+#Bonus: Organizar por ordem alfabetica da ultima letra do nome
+
+
+
+lista = ['Alexandre', 'Alice', 'André', 'Arthur', 'Arthur', 'Artur', 'Augusto', 'Bernardo', 'Bernardo', 'Bruno', 'Davi', 'Diego', 'Eduardo', 'Fabrício', 'Felipe', 'Fernando', 'Francisco', 'Francisco', 'Gabriel', 'Gabriel', 'Giovanna', 'Giovanni', 'Guilherme', 'Guilherme', 'Hector', 'Henrique', 'Inácio', 'João', 'João', 'Joaquim', 'Júlia', 'Lauren', 'Leonardo', 'Leonardo', 'Lucas', 'Marina', 'Matheus', 'Matheus', 'Paula', 'Pedro', 'Pedro', 'Pedro', 'Pedro', 'Rafael', 'Regis', 'Sofia', 'Stella', 'Thiago', 'Valentina', 'Vicente', 'Lucas']
+
+lista.sort()
+for nome in lista:
+    print(nome)
